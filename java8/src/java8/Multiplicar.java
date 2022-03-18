751a61af44e445614080b5141e9c35fa72700796
vitropez <64587299+vitropez@users.noreply.github.com>
2022-03-18 10:25:32 +0100

    Add files via upload

---
+++ java8/src/java8/Multiplicar.java
@@ -0,0 +1,8 @@
+package java8;
+
+
+@FunctionalInterface
+public interface Multiplicar {
+	public int multiplicar(int a);
+
+}
---
+++ java8/src/java8/Principal.java
@@ -0,0 +1,12 @@
+package java8;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Multiplicar d=b->b*2;
+		int result=d.multiplicar(4);
+		System.out.println(result);
+
+	}
+
+}
